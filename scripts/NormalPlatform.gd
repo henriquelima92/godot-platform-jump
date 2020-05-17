23852f655a31b7a5436e7718e30120d119b0ed1b
henriquelima92 <henriquedelima92@hotmail.com>
2020-05-17 11:12:59 -0300

    Added player camera movement and platforms spawn system.

--- scripts/NormalPlatform.gd
+++ scripts/NormalPlatform.gd
@@ -1,13 +1,10 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
+onready var sprite = $Sprite
+var sprite_half_width
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	sprite_half_width = sprite.texture.get_size().x / 2 * scale.x
 	connect("body_entered", self, "_on_body_entered")
 	pass # Replace with function body.
 
